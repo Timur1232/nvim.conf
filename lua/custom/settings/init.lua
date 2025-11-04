edefdd6b130ae359b4df480ad6ba64591a028c1f
Timur Baimuradov Rashidovich <timtimch123@gmail.com>
2025-11-04 23:04:27 +0300

    poopoo peepee

--- lua/custom/settings/init.lua
+++ lua/custom/settings/init.lua
@@ -1,6 +1,7 @@
 return {
 	require("custom.settings.harpoon"),
     require("custom.settings.lint"),
-    require("custom.settings.tinymist"),
+    require("custom.settings.autotag"),
+    -- require("custom.settings.tinymist"),
     -- require("custom.settings.typst-preview"),
 }
--- lua/custom/settings/tinymist.lua
+++ lua/custom/settings/tinymist.lua
@@ -1,9 +1,9 @@
-vim.lsp.config["tinymist"] = {
-	cmd = { "tinymist" },
-	filetypes = { "typst" },
-	settings = {
-		formatterMode = "typstyle",
-		exportPdf = "onType",
-		semanticTokens = "disable",
-	},
-}
+-- vim.lsp.config["tinymist"] = {
+-- 	cmd = { "tinymist" },
+-- 	filetypes = { "typst" },
+-- 	settings = {
+-- 		formatterMode = "typstyle",
+-- 		exportPdf = "onType",
+-- 		semanticTokens = "disable",
+-- 	},
+-- }
